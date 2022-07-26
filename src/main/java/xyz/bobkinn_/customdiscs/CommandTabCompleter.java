6b5fc7e4c953e2338105a2e0925f388a4c6c1efd
BoBkiNN_ <bobkin.yangirov@gmail.com>
2022-07-26 14:21:23 +0600

    update

--- src/main/java/xyz/bobkinn_/customdiscs/CommandTabCompleter.java
+++ src/main/java/xyz/bobkinn_/customdiscs/CommandTabCompleter.java
@@ -3,7 +3,9 @@ package xyz.bobkinn_.customdiscs;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -12,8 +14,23 @@ public class CommandTabCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         if (args.length==1){
-            sender.sendMessage("1");
             return Arrays.asList("list","reload","del","add","get","play");
+        }
+        if (args.length==2 && args[1].equalsIgnoreCase("get")){
+            ArrayList<String> ids = new ArrayList<>();
+            int c = 0;
+            for (CustomDisc ignored : Main.customDiscs){
+                c++;
+                ids.add(String.valueOf(c));
+            }
+            return ids;
+        }
+        if (args.length==3 && args[1].equalsIgnoreCase("get")){
+            ArrayList<String> players = new ArrayList<>();
+            for (Player p : sender.getServer().getOnlinePlayers()){
+                players.add(p.getName());
+            }
+            return players;
         }
         return Collections.singletonList("");
     }
